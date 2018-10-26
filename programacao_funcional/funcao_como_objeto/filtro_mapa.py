c2e40dc3a383097d34f494520a9bc23f76f82a9c
Lucas Polo <pradopolo@gmail.com>
2018-10-26 16:12:02 -0300

    Map

--- programacao_funcional/funcao_como_objeto/filtro_mapa.py
+++ programacao_funcional/funcao_como_objeto/filtro_mapa.py
@@ -2,6 +2,7 @@ alunos = [('Renzo', 0), ('Luciano', 10), ('Ada', 9)]
 
 print([(nome, nota) for nome, nota in alunos if nota > 5])
 
+
 # Pode-se fazer o mesmo usando filter
 def possui_nota_maior_que_5(aluno):
     _, nota = aluno
@@ -9,3 +10,12 @@ def possui_nota_maior_que_5(aluno):
 
 
 print(list(filter(possui_nota_maior_que_5, alunos)))
+
+
+# Agora vamos apenas extrair apenas o nome
+def extrair_nome(aluno):
+    nome, _ = aluno
+    return nome
+
+
+print(list(map(extrair_nome, filter(possui_nota_maior_que_5, alunos))))
