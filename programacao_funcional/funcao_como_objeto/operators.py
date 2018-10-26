9b7115bcd7a61a2049c04a3de7f309953cee6dbf
Lucas Polo <pradopolo@gmail.com>
2018-10-26 16:28:46 -0300

    Operators e exercícios

---
+++ programacao_funcional/funcao_como_objeto/operators.py
@@ -0,0 +1,5 @@
+import operator  # Lib com funções auxiliares para passar por parametro
+
+pegar_primeiro = operator.getitem(0)
+
+print(pegar_primeiro[1,2,3,4,5])  # Como descrito, irá pegar o primeiro indíce
